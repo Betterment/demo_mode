ca7be5f192b85573dd059e52f03a65ee44becb3a
Ray Zane <raymondzane@gmail.com>
2024-07-22 09:37:19 -0700

    Validate persona on create

--- app/models/demo_mode/session.rb
+++ app/models/demo_mode/session.rb
@@ -5,6 +5,7 @@ module DemoMode
     attribute :variant, default: :default
 
     validates :persona_name, :variant, presence: true
+    validate :persona_must_exist, on: :create
     belongs_to :signinable, polymorphic: true, optional: true
 
     before_create :set_password!
@@ -28,6 +29,10 @@ module DemoMode
     # Heads up: finding a persona is not guaranteed (e.g. past sessions)
     def persona
       DemoMode.personas.find { |p| p.name.to_s == persona_name.to_s }
+    end
+
+    def persona_must_exist
+      errors.add(:persona_name, 'does not exist') if persona_name? && persona.nil?
     end
   end
 end
--- spec/models/demo_mode/session_spec.rb
+++ spec/models/demo_mode/session_spec.rb
@@ -5,16 +5,27 @@ require 'spec_helper'
 RSpec.describe DemoMode::Session do
   include ActiveJob::TestHelper
 
-  it 'is valid at rest' do
-    subject.persona_name = :the_everyperson
-    subject.variant = 'XIV'
-    expect(subject).to be_valid
-  end
-
   it 'validates persona name' do
     subject.persona_name = nil
     expect(subject).not_to be_valid
     expect(subject.errors.full_messages).to match_array("Persona name can't be blank")
+  end
+
+  it 'validates persona exists on create' do
+    subject.persona_name = 'garbage'
+    expect(subject).not_to be_valid
+    expect(subject.errors.full_messages).to match_array("Persona name does not exist")
+  end
+
+  it 'allows persisted records to reference non-existent personas' do
+    subject.persona_name = :the_everyperson
+    subject.variant = 'XIV'
+
+    expect(subject).not_to be_valid
+    expect(subject.errors.full_messages).to match_array("Persona name does not exist")
+
+    subject.save!(validate: false) # bypass validations on create
+    expect(subject).to be_valid
   end
 
   it 'validates persona variant' do
@@ -45,45 +56,6 @@ RSpec.describe DemoMode::Session do
         expect(subject.begin_demo).to be_present
         expect { subject.begin_demo.call }
           .to raise_error "I'm afraid I can't do that, Dave."
-      end
-    end
-  end
-
-  describe '#save' do
-    subject { described_class.new(persona_name: :the_everyperson) }
-
-    it 'raises an error when the persona is not recognized' do
-      expect(subject.persona_name).to eq 'the_everyperson'
-      perform_enqueued_jobs do
-        expect { subject.save! }.to raise_error(RuntimeError, 'Unknown persona: the_everyperson')
-      end
-    end
-
-    context 'when the persona is known' do
-      let(:random_name) { SecureRandom.hex }
-
-      before do
-        name = random_name
-        DemoMode.configure do
-          persona :the_everyperson do
-            features << 'test'
-
-            sign_in_as { DummyUser.create!(name: name) }
-          end
-        end
-      end
-
-      after do
-        DemoMode.send(:remove_instance_variable, '@configuration')
-        load Rails.root.join('config/initializers/demo_mode.rb')
-      end
-
-      it 'generates a new account and assigns it to the session' do
-        perform_enqueued_jobs do
-          expect { subject.save! }.to change { described_class.first&.signinable&.name }.from(nil).to(random_name)
-          expect(described_class.count).to eq 1
-          expect(DummyUser.count).to eq 1
-        end
       end
     end
   end
