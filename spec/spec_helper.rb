0ae33c65661ac50ff4fdc7b8c1fff6123482f672
Irving Caro <irving.caro@betterment.com>
2025-01-22 14:01:42 -0600

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,26 +20,14 @@ end
 require 'rspec/rails'
 require 'capybara/cuprite'
 
-Capybara.register_driver(:better_cuprite) do |app|
-  browser_options = ENV.fetch('CI', nil) ? { 'no-sandbox': nil } : {}
-
-  options = {
-    window_size: [1280, 1024],
-    headless: ENV['CAPYBARA_DEBUG'] != '1',
-    process_timeout: 20,
-    js_errors: true,
-    browser_options: browser_options,
-  }
-
-  Capybara::Cuprite::Driver.new(app, **options)
-end
+Capybara.default_driver = Capybara.javascript_driver = :cuprite
 
 Capybara.configure do |config|
   config.match = :one
   config.ignore_hidden_elements = true
   config.visible_text_only = true
-  config.default_driver = :better_cuprite
-  config.javascript_driver = :better_cuprite
+  config.default_driver = :cuprite
+  config.javascript_driver = :cuprite
 
   config.default_max_wait_time = ENV.fetch('CAPYBARA_WAIT_TIME', 2).to_i
   config.server = :webrick
@@ -54,7 +42,17 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.before(:each, type: :system) do
-    driven_by :better_cuprite
+    browser_options = ENV.fetch('CI', nil) ? { 'no-sandbox': nil } : {}
+
+    options = {
+      window_size: [1280, 1024],
+      headless: ENV['CAPYBARA_DEBUG'] != '1',
+      process_timeout: 20,
+      js_errors: true,
+      browser_options: browser_options,
+    }
+
+    driven_by Capybara.javascript_driver, options: options
   end
 
   # Reset configuration
