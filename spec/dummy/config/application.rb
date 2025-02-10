a057fe3fafc0217ac6055725dcd049d93ad28a63
Ray Zane <raymondzane@gmail.com>
2025-02-10 11:01:11 -0500

    Setup sprockets

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -5,6 +5,7 @@ require 'action_view/railtie'
 require 'active_job/railtie'
 require 'active_model/railtie'
 require 'active_record/railtie'
+require 'sprockets/railtie'
 
 Bundler.require(:default, Rails.env)
 
