d71ad09e958b7ef89b64476a8ac02fec533fadaf
Josh Sacks <josh.sacks@betterment.com>
2025-02-11 15:04:13 -0800

    move blank persona error into try/catch block

--- app/jobs/demo_mode/account_generation_job.rb
+++ app/jobs/demo_mode/account_generation_job.rb
@@ -7,15 +7,13 @@ module DemoMode
         persona = session.persona
         raise "Unknown persona: #{session.persona_name}" if persona.blank?
 
-        begin
-          signinable = persona.generate!(variant: session.variant, password: session.signinable_password, options: options)
-          if signinable.present?
-            session.update!(signinable: signinable, status: 'successful')
-          end
-        rescue StandardError => e
-          session.update!(status: 'failed')
-          Rails.logger.error(e)
+        signinable = persona.generate!(variant: session.variant, password: session.signinable_password, options: options)
+        if signinable.present?
+          session.update!(signinable: signinable, status: 'successful')
         end
+      rescue StandardError => e
+        session.update!(status: 'failed')
+        Rails.logger.error(e)
       end
       raise "Failed to create signinable persona!" if session.signinable.blank?
     end
