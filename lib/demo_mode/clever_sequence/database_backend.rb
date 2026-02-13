1c845fe9ab00a63de77cbc0c296506b4d379e791
claire davis <claire.davis@betterment.com>
2026-02-13 16:33:07 -0500

    fix up the db backend file

--- lib/demo_mode/clever_sequence/database_backend.rb
+++ lib/demo_mode/clever_sequence/database_backend.rb
@@ -4,15 +4,35 @@ class CleverSequence
   module DatabaseBackend
     SEQUENCE_PREFIX = 'clever_seq_'
 
+    THROW_IF_SEQUENCE_NOT_FOUND = true
+
+    class SequenceNotFoundError < StandardError
+      attr_reader :sequence_name, :klass, :attribute
+
+      def initialize(sequence_name:, klass:, attribute:)
+        @sequence_name = sequence_name
+        @klass = klass
+        @attribute = attribute
+        super("Sequence '#{sequence_name}' not found for #{klass.name}##{attribute}. Run migration to create sequences.")
+      end
+    end
+
     class << self
       def nextval(klass, attribute, block)
         name = sequence_name(klass, attribute)
-        ensure_sequence_exists!(klass, attribute, name, block)
 
         result = ActiveRecord::Base.connection.execute(
           "SELECT nextval('#{name}')",
         )
         result.first['nextval'].to_i
+      rescue ActiveRecord::StatementInvalid => e
+        if sequence_not_exists_error?(e)
+          return calculate_sequence_value(klass, attribute, block) + 1 unless THROW_IF_SEQUENCE_NOT_FOUND
+
+          raise SequenceNotFoundError.new(sequence_name: name, klass: klass, attribute: attribute)
+        else
+          raise # Re-raise other database errors
+        end
       end
 
       def sequence_name(klass, attribute)
@@ -21,79 +41,17 @@ class CleverSequence
         "#{SEQUENCE_PREFIX}#{table}_#{attr}"[0, 63] # PostgreSQL identifier limit
       end
 
-      def ensure_sequence_exists!(klass, attribute, name, block)
-        return if sequence_verified?(name)
-
-        create_sequence_if_not_exists(klass, attribute, name, block)
-        mark_sequence_verified!(name)
-      end
-
-      def reset_verified_sequences!
-        verified_sequences.clear
-      end
-
-      def drop_all_sequences!
-        ActiveRecord::Base.connection.execute(<<-SQL.squish)
-          DO $$
-          DECLARE
-            seq_name TEXT;
-          BEGIN
-            FOR seq_name IN
-              SELECT sequence_name
-              FROM information_schema.sequences
-              WHERE sequence_name LIKE '#{SEQUENCE_PREFIX}%'
-            LOOP
-              EXECUTE 'DROP SEQUENCE IF EXISTS ' || seq_name;
-            END LOOP;
-          END $$;
-        SQL
-        reset_verified_sequences!
-      end
-
       private
 
-      def verified_sequences
-        @verified_sequences ||= Concurrent::Set.new
+      def sequence_not_exists_error?(error)
+        error.cause.is_a?(PG::UndefinedTable) # todo: Nathan tell us if this is right
       end
 
-      def sequence_verified?(name)
-        verified_sequences.include?(name)
-      end
-
-      def mark_sequence_verified!(name)
-        verified_sequences.add(name)
-      end
-
-      def create_sequence_if_not_exists(klass, attribute, name, block)
-        starting_value = calculate_starting_value(klass, attribute, block)
-
-        ActiveRecord::Base.connection.execute(<<-SQL.squish)
-          DO $$
-          BEGIN
-            IF NOT EXISTS (
-              SELECT 1 FROM information_schema.sequences
-              WHERE sequence_name = '#{name}'
-            ) THEN
-              CREATE SEQUENCE #{name} START WITH #{starting_value + 1};
-            END IF;
-          END $$;
-        SQL
-      rescue ActiveRecord::StatementInvalid => e
-        # Handle race condition where another process created it
-        raise unless e.message.include?('already exists')
-      end
-
-      def calculate_starting_value(klass, attribute, block)
+      def calculate_sequence_value(klass, attribute, block)
         column_name = klass.attribute_aliases[attribute.to_s] || attribute.to_s
         return 0 unless klass.column_names.include?(column_name)
 
-        # For integer columns, use MAX for efficiency
-        if klass.columns_hash[column_name]&.type == :integer
-          klass.maximum(column_name) || 0
-        else
-          # Fall back to LowerBoundFinder for non-integer columns
-          LowerBoundFinder.new(klass, column_name, block).lower_bound
-        end
+        LowerBoundFinder.new(klass, column_name, block).lower_bound
       end
     end
   end
