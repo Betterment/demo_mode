0e53ed5caaf281542f3606b780a1a0857f9410d0
Brandon Trautmann <8343465+btrautmann@users.noreply.github.com>
2026-03-26 17:08:26 -0400

    fix: rubocop issues

--- db/migrate/20260326000000_add_claimed_at_to_demo_mode_sessions.rb
+++ db/migrate/20260326000000_add_claimed_at_to_demo_mode_sessions.rb
@@ -5,7 +5,7 @@ class AddClaimedAtToDemoModeSessions < ActiveRecord::Migration[5.1]
     add_column :demo_mode_sessions, :claimed_at, :datetime
 
     add_index :demo_mode_sessions,
-      [:persona_name, :variant, :status, :claimed_at],
+      %i[persona_name variant status claimed_at],
       name: :index_demo_mode_sessions_on_pool_lookup
   end
 end
