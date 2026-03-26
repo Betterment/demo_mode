1e6b6c05dfe52616d7d76e7debc64a56bc4b4571
Brandon Trautmann <8343465+btrautmann@users.noreply.github.com>
2026-03-26 17:09:47 -0400

    fix: dummy schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_03_26_000000) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
+ActiveRecord::Schema.define(version: 2026_03_26_000000) do
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer "attempts", default: 0, null: false
     t.datetime "created_at", null: false
