110618966137d6edd175776f349b93a495d2608b
Brandon Trautmann <8343465+btrautmann@users.noreply.github.com>
2026-03-27 14:17:18 -0400

    use concurrent algorithm when creating index

--- db/migrate/20260326000000_add_claimed_at_to_demo_mode_sessions.rb
+++ db/migrate/20260326000000_add_claimed_at_to_demo_mode_sessions.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 class AddClaimedAtToDemoModeSessions < ActiveRecord::Migration[5.1]
+  disable_ddl_transaction!
+
   def change
     add_column :demo_mode_sessions, :claimed_at, :datetime
 
     add_index :demo_mode_sessions,
       %i(persona_name variant status claimed_at),
-      name: :index_demo_mode_sessions_on_pool_lookup
+      name: :index_demo_mode_sessions_on_pool_lookup,
+      algorithm: :concurrently
   end
 end
