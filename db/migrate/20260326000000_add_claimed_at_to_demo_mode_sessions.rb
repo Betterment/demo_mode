7c15ddff967cbe565c2a4113f1d88ae3804be3fb
Brandon Trautmann <8343465+btrautmann@users.noreply.github.com>
2026-03-27 14:30:26 -0400

    ensure all existing personas are non-pool

--- db/migrate/20260326000000_add_claimed_at_to_demo_mode_sessions.rb
+++ db/migrate/20260326000000_add_claimed_at_to_demo_mode_sessions.rb
@@ -6,6 +6,10 @@ class AddClaimedAtToDemoModeSessions < ActiveRecord::Migration[5.1]
   def change
     add_column :demo_mode_sessions, :claimed_at, :datetime
 
+    reversible do |dir|
+      dir.up { execute "UPDATE demo_mode_sessions SET claimed_at = created_at" }
+    end
+
     add_index :demo_mode_sessions,
       %i(persona_name variant status claimed_at),
       name: :index_demo_mode_sessions_on_pool_lookup,
