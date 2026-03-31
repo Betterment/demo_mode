539d411289dd18b23e82c5b55518f07a7c1e16ac
Brandon Trautmann <btrautmann@users.noreply.github.com>
2026-03-31 12:35:48 -0400

    Update db/migrate/20260331000000_rename_successful_to_available_in_demo_mode_sessions.rb
    Co-authored-by: Nathan G <nathan@ngriffith.com>

--- db/migrate/20260331000000_rename_successful_to_available_in_demo_mode_sessions.rb
+++ db/migrate/20260331000000_rename_successful_to_available_in_demo_mode_sessions.rb
@@ -2,10 +2,10 @@
 
 class RenameSuccessfulToAvailableInDemoModeSessions < ActiveRecord::Migration[5.1]
   def up
-    execute "UPDATE demo_mode_sessions SET status = 'available' WHERE status = 'successful'"
+    execute "UPDATE demo_mode_sessions SET status = 'in_use' WHERE status = 'successful'"
   end
 
   def down
-    execute "UPDATE demo_mode_sessions SET status = 'successful' WHERE status = 'available'"
+    execute "UPDATE demo_mode_sessions SET status = 'successful' WHERE status = 'in_use'"
   end
 end
