0747409557a59a634eb9f085378a9572efefeb93
Brandon Trautmann <8343465+btrautmann@users.noreply.github.com>
2026-04-02 12:58:55 -0400

    fix: various migration issues seen when running migrations for consuming app

--- db/migrate/20260326000000_add_claimed_at_to_demo_mode_sessions.rb
+++ db/migrate/20260326000000_add_claimed_at_to_demo_mode_sessions.rb
@@ -7,12 +7,14 @@ class AddClaimedAtToDemoModeSessions < ActiveRecord::Migration[5.1]
     add_column :demo_mode_sessions, :claimed_at, :datetime
 
     reversible do |dir|
-      dir.up { execute "UPDATE demo_mode_sessions SET claimed_at = created_at" }
+      dir.up { safety_assured { execute "UPDATE demo_mode_sessions SET claimed_at = created_at" } }
     end
 
-    add_index :demo_mode_sessions,
-      %i(persona_name variant status claimed_at),
-      name: :index_demo_mode_sessions_on_pool_lookup,
-      algorithm: :concurrently
+    safety_assured do
+      add_index :demo_mode_sessions,
+        %i(persona_name variant status claimed_at),
+        name: :index_demo_mode_sessions_on_pool_lookup,
+        algorithm: :concurrently
+    end
   end
 end
--- db/migrate/20260331000000_rename_successful_to_available_in_demo_mode_sessions.rb
+++ db/migrate/20260331000000_rename_successful_to_available_in_demo_mode_sessions.rb
@@ -2,10 +2,10 @@
 
 class RenameSuccessfulToAvailableInDemoModeSessions < ActiveRecord::Migration[5.1]
   def up
-    execute "UPDATE demo_mode_sessions SET status = 'in_use' WHERE status = 'successful'"
+    safety_assured { execute "UPDATE demo_mode_sessions SET status = 'in_use' WHERE status = 'successful'" }
   end
 
   def down
-    execute "UPDATE demo_mode_sessions SET status = 'successful' WHERE status = 'in_use'"
+    safety_assured { execute "UPDATE demo_mode_sessions SET status = 'successful' WHERE status = 'in_use'" }
   end
 end
