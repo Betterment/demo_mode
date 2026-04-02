832d6caedc61baefa733741e096906226dd41f10
Brandon Trautmann <8343465+btrautmann@users.noreply.github.com>
2026-04-02 13:00:38 -0400

    rename migration file to reflect its purpose

--- db/migrate/20260331000000_rename_successful_to_in_use_in_demo_mode_sessions.rb
+++ db/migrate/20260331000000_rename_successful_to_in_use_in_demo_mode_sessions.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RenameSuccessfulToAvailableInDemoModeSessions < ActiveRecord::Migration[5.1]
+class RenameSuccessfulToInUseInDemoModeSessions < ActiveRecord::Migration[5.1]
   def up
     safety_assured { execute "UPDATE demo_mode_sessions SET status = 'in_use' WHERE status = 'successful'" }
   end
